7f90760486c9680d50893fae060e2fe9607ac3d2
TheWoodenMan <andynwood@googlemail.com>
2022-11-17 02:11:36 +0000

    add values implementation in progress

--- controllers/api.js
+++ controllers/api.js
@@ -67,7 +67,7 @@ module.exports = {
 				console.log(err);
 			});
 	},
-	replaceQuestionById: (req, res) => {
+	replaceByQuestionId: (req, res) => {
 		const id = req.params.id;
 		const body = req.body;
 		// questions.find({id: values})
@@ -88,11 +88,53 @@ module.exports = {
 		const values = req.body.values;
 		console.log(`values: ${values}`);
 		// add the document to a variable
-		// routerly the new values to that document
+		// copy the new values to that document
 		// save the document and return the results.
 		const question = Question.findOne({ "_id": ObjectId(`${id}`) });
 		console.log(question);
 		question.values = values;
+
+		Question.findOne({ "_id": ObjectId(`${id}`) })
+			.replaceOne(question)
+			.then((results) => {
+				console.log(`{ _id : ObjectID(${id}) } Values updated`);
+				console.log(results);
+				res.status(200);
+				res.json(results);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	},
+	addValueByQuestionId: (req, res) => {
+		// route: /api/questions/:id/:value/addvalue
+		const id = req.params.id;
+		const value = req.params.value;
+		let arrayIncludes;
+		console.log(value);
+		console.log(`adding value: ${value} to question: ${id}`);
+		// add the document to a variable
+		// copy the new values to that document
+		// save the document and return the results.
+		let question;
+		try {
+			// note to self - the problem is that the question isn't being returned.
+			question = Question.findOne({ "_id": ObjectId(`${id}`) });
+
+			arrayIncludes = question.values.includes(value);
+		} catch (err) {
+			console.log(err);
+			res.send(err);
+		}
+
+		console.log(question);
+
+		// check if the value already exists in the array, if it does - return an error code, if it doesn't - add it to the array.
+		!arrayIncludes || question.values.length === 0
+			? question.values.push(value)
+			: res.status(400);
+
+		console.log(question);
 
 		Question.findOne({ "_id": ObjectId(`${id}`) })
 			.replaceOne(question)
--- routes/api.js
+++ routes/api.js
@@ -7,12 +7,18 @@ router.post("/questions/addmany", apiController.addManyQuestions);
 router.get("/questions/:id/", apiController.getQuestionById);
 
 // receives the object in the body and replaces the id in the params with the new object.
-router.post("/questions/:id/replace/", apiController.replaceQuestionById);
+router.post("/questions/:id/replace/", apiController.replaceByQuestionId);
 
 // searches for a question by id then replaces the values with the body of the request
 router.post(
 	"/questions/:id/values/replace",
 	apiController.replaceValuesByQuestionId
+);
+
+// adds one value as a string to the array of values, based on question ID number
+router.patch(
+	"/questions/:id/:value/addvalue/",
+	apiController.addValueByQuestionId
 );
 
 // searches the db values arrays and displays each full record that matches
