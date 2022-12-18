72bd52a38ee7d72cc66bce70f37dd61fc4eb2857
TheWoodenMan <andynwood@googlemail.com>
2022-12-18 01:48:19 +0000

    tweaked jest settings

---
+++ app.js
@@ -0,0 +1,35 @@
+//app.js
+const express = require("express");
+const app = express();
+const db = require("./config/database");
+ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const cors = require("cors");
+
+// ************** express settings **************
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
+app.use(logger("dev"));
+app.use(cors());
+
+// ************** DB Commands *******************
+
+const connectDB = db.connectDB;
+
+// Connect To Database
+connectDB();
+
+// ************** Routes ************************
+
+const apiRoutes = require("./routes/api");
+const pageRoutes = require("./routes/page");
+
+app.use("/", pageRoutes);
+app.use("/api", apiRoutes);
+
+module.exports = app;
---
+++ app.test.js
@@ -0,0 +1,89 @@
+const request = require("supertest");
+const app = require("./server");
+const db = require("./config/database");
+
+describe("Load Pages", () => {
+	it("GET / should return and render index.ejs to html", (done) => {
+		const res = request(app)
+			.get("/")
+			.expect("Content-Type", /text\/html/)
+			.expect(200)
+			.end(function (err, res) {
+				if (err) return done(err);
+				done();
+			});
+	});
+	it("GET /random should return random.ejs", (done) => {
+		const res = request(app)
+			.get("/random")
+			.expect("Content-Type", /text\/html/)
+			.expect(200)
+			.end(function (err, res) {
+				if (err) return done(err);
+				done();
+			});
+	});
+	it("GET /questionList should return questionList.ejs", (done) => {
+		const res = request(app)
+			.get("/questionList")
+			.expect("Content-Type", /text\/html/)
+			.expect(200)
+			.end(function (err, res) {
+				if (err) return done(err);
+				done();
+			});
+	});
+	it("GET /values should return values.ejs", (done) => {
+		const res = request(app)
+			.get("/values")
+			.expect("Content-Type", /text\/html/)
+			.expect(200)
+			.end(function (err, res) {
+				if (err) return done(err);
+				done();
+			});
+	});
+	it("GET /questionForm should return questionForm.ejs", (done) => {
+		const res = request(app)
+			.get("/questionForm")
+			.expect("Content-Type", /text\/html/)
+			.expect(200)
+			.end(function (err, res) {
+				if (err) return done(err);
+				done();
+			});
+	});
+});
+
+describe("GET Page Functions", () => {
+	it("GET /randomSearch should return up to 10 random records", () => {});
+	it("GET /valueSearch should return all records by a given value from form input and button push", () => {});
+	it("GET /questionList should return questionList.ejs", () => {});
+	it("GET /values should return values.ejs", () => {});
+	it("GET /questionForm should return questionForm.ejs", () => {});
+});
+
+describe("POST Page Functions", () => {
+	it("POST /submitQuestion with a question in the body should create a new Question documents", () => {});
+});
+
+describe("DELETE Page Functions", () => {
+	it("DELETE /deleteQuestion should delete a question by id after button push", () => {});
+});
+
+describe("Safe API Calls GET", () => {
+	it("GET /questions/:id/ with an id number in the params should get that document", () => {});
+	it("GET /questions/value/:value with a value in the params should get all questions with that value", () => {});
+	it("GET /questions/random/:number with a number in the params should get that many random questions", () => {});
+});
+
+describe("Restricted API Calls POST", () => {
+	it("POST /questions/addone with JSON in the body should create a new document", () => {});
+	it("POST /questions/addmany with an array of JSONs in the body should create multiple new document", () => {});
+	it("POST /questions/:id/replace/ with an id in the params and a JSON in the body should keep the same id and replace the question/values", () => {});
+	it("POST /questions/:id/values/replace with an id and value in the params and a JSON in the body should keep the same id and question, replacing only the values", () => {});
+});
+
+describe("Restricted API Calls DELETE", () => {
+	it("DELETE /questions/:id/delete/ with an id in the params should delete that question", () => {});
+});
