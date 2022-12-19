fbb0297adfed29449ecc993eae7e1145a0d1015a
TheWoodenMan <andynwood@googlemail.com>
2022-12-19 23:54:09 +0000

    this commit fixes two issues with the client UI First the recycle button now correctly picks a random question and replaces it from the db second the questionForm now correctly renders and allows question submission the questionForm page also now renders the question you just submitted

--- public/js/main.js
+++ public/js/main.js
@@ -10,22 +10,71 @@ Array.from(trashSymbol).forEach((el) => {
 });
 
 async function replaceQuestion() {
-	const questionText = this.parentNode.childNodes[1].innerText;
+	let data;
 
-	try {
-		const response = await fetch("/replaceQuestion", {
-			method: "post",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({
-				"question": questionText
-			})
+	const createNewLi = function (id, question) {
+		const newLi = document.createElement("li");
+		const newSpan = document.createElement("span");
+
+		newSpan.id = id;
+		newSpan.innerText = question;
+
+		const iRec = document.createElement("i");
+		const iTrash = document.createElement("i");
+
+		iRec.classList.add("fa-solid");
+		iRec.classList.add("fa-recycle");
+
+		iTrash.classList.add("fa-solid");
+		iTrash.classList.add("fa-trash");
+
+		iRec.addEventListener("click", replaceQuestion);
+		iTrash.addEventListener("click", deleteQuestion);
+
+		newSpan.appendChild(iRec);
+		newSpan.appendChild(iTrash);
+
+		newLi.appendChild(newSpan);
+
+		return newLi;
+	};
+
+	const getRandomQuestion = async function () {
+		const returnObj = {};
+
+		try {
+			const response = await fetch("/replaceQuestion", {
+				method: "post",
+				headers: { "Content-Type": "application/json" }
+			});
+
+			if (!response.ok) {
+				throw new Error(`Error! status: ${response.status}`);
+			}
+
+			const result = await response.json();
+
+			returnObj._id = result._id;
+			returnObj.question = result.question + " ";
+		} catch (err) {
+			console.log(err);
+		}
+		console.log(returnObj);
+		return returnObj;
+	};
+
+	getRandomQuestion()
+		.then((data) => {
+			console.log(data);
+			const newLi = createNewLi(data._id, data.question);
+
+			this.parentNode.after(newLi);
+
+			this.parentNode.remove();
+		})
+		.catch((err) => {
+			console.log(err);
 		});
-		const data = await response.json();
-		console.log(data);
-		location.reload();
-	} catch (err) {
-		console.log(err);
-	}
 }
 
 async function deleteQuestion() {
